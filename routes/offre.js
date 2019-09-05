5f52bb9e98cf8bafa3bc369763ee2e010e1eb7f7
benhakimilyass <ilyassbenhakim2@gmail.com>
2019-09-05 15:25:46 +0100

    url offre

--- routes/offre.js
+++ routes/offre.js
@@ -1,65 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mongoose = require('mongoose');
-var Offre = require('../models/offre');
-const User = require('../models/user');
-var ObjectID = require('mongodb').ObjectID;
-
-
-
-
+var mongoose = require("mongoose");
+var Offre = require("../models/offre");
+const User = require("../models/user");
+var ObjectID = require("mongodb").ObjectID;
 
 /* GET ALL offres */
-router.get('/offres', function(req, res, next) {
-  Offre.find(function (err, products) {
+router.get("/offres", function(req, res, next) {
+  Offre.find(function(err, products) {
     if (err) return next(err);
     res.json(products);
   });
 });
 
-
 /* SAVE offre */
-router.post('/save', function(req, res, next) {
-
-  Offre.create(req.body, function (err, offre) {
+router.post("/saveOffre", function(req, res, next) {
+  Offre.create(req.body, function(err, offre) {
     if (err) return next(err);
     res.json(offre);
-    })
-    });
-
-
-
+  });
+});
 
 /* GET  offres BY ID CREATOR   */
-router.get('/offres/:id', function(req, res, next) {
-    Offre.collection.find({idcreateur: req.params.id}).toArray((err, items) => {
-        console.log(items);
-        res.json(items);
-      })
-      
+router.get("/offres/:id", function(req, res, next) {
+  Offre.collection.find({ idcreateur: req.params.id }).toArray((err, items) => {
+    console.log(items);
+    res.json(items);
   });
-
-  
-
-
+});
 
 /* UPDATE offre */
-router.put('/offre/:id', function(req, res, next) {
-    Offre.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+router.put("/offre/:id", function(req, res, next) {
+  Offre.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
     if (err) return next(err);
     res.json(post);
   });
 });
 
 /* DELETE offre */
-router.delete('/offre/:id', function(req, res, next) {
-    Offre.findByIdAndRemove(req.params.id, function (err, post) {
+router.delete("/offre/:id", function(req, res, next) {
+  Offre.findByIdAndRemove(req.params.id, function(err, post) {
     if (err) return next(err);
     res.json(post);
   });
-
 });
-
-
 
 module.exports = router;
